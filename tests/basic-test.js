f23a7ae8b85b509c6f3297a1e2bcb06c3656d9c1
parker-gary <gary.parker@wintechnologies.net>
2020-04-18 13:49:51 +0100

    remove comment

--- tests/basic-test.js
+++ tests/basic-test.js
@@ -8,6 +8,5 @@ test('My first test', async t => {
         .typeText('#developer-name', 'John Smith')
         .click('#submit-button')
 
-        // Use the assertion to check if the actual header text is equal to the expected one
         .expect(Selector('#article-header').innerText).eql('Thank you, John Smith!');
 });
